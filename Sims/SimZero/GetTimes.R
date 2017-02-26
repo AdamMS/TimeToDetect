471be6b823028fea1a4eb83e490ef7e80529fe39
Adam Martin-Schwarze <adamms@iastate.edu>
2017-02-26 12:59:14 -0600

    Code to extract run times from model fits.

---
+++ Sims/SimZero/GetTimes.R
@@ -0,0 +1,27 @@
+##### Gets Run Times
+library(rstan)
+simtable <- read.csv("S0_simtable.csv")
+nReps    <- 1
+runtimes <- data.frame(matrix(NA, nrow=nrow(simtable), ncol=nReps))
+for(Rep in 1:nReps){
+  for(m in 1:nrow(simtable)){
+    loadfile <- paste0("Sim_", Rep, "/", simtable[m,"fname"], ".Rdata")
+    if(file.exists(loadfile)){
+      load(loadfile)
+      runtimes[m, Rep] <- sum(get_elapsed_time(eval(parse(text=as.character(simtable[m,"modeloutput"])))))
+    } else {
+      runtimes[m,Rep] <- NA}
+  }
+}
+saveRDS(runtimes, file="RunTimes.rds")
+
+# ##### Extracts Run Times
+# library(plyr)
+# library(reshape2)
+# simtable <- read.csv("S0_simtable.csv")
+# runtimes <- readRDS("RunTimes.rds")
+# runtimes <- cbind(simtable[,1:2], runtimes)
+# r.melt <- melt(runtimes, id.vars=1:2, value.name="runtime")
+# smurf <- ddply(r.melt, .(modelname), summarize, mu.t=mean(runtime))
+# smurf$minutes <- smurf$mu.t/60
+# smurf
